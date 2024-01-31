e8dbe14bd0065ef5d55711febb638b6741700d3a
joaogabrielantune <joaogabrielantunesdefaria@gmail.com>
2024-01-31 19:20:54 -0300

    fixs

--- src/main/java/org/example/ExercicioIdade.java
+++ src/main/java/org/example/ExercicioIdade.java
@@ -1,2 +1,25 @@
-package org.example;public class ExercicioIdade {
+package org.example;
+
+import java.lang.System;
+
+public class ExercicioIdade {
+
+    int idade;
+
+    public void imprimir(){
+        if (idade >= 18){
+            System.out.println("Maior de Idade");
+        }else{
+            System.out.println("Menor de idade");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        ExercicioIdade idade = new ExercicioIdade();
+
+        idade.idade = 17;
+
+        idade.imprimir();
+    }
 }
